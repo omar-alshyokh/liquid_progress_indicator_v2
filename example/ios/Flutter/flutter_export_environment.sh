fe601ecb491a5d94ec0a1f0ae532a31f45c65c51
OmMAar <omar.alshyokh@gmail.com>
2023-08-15 00:25:59 +0400

    change flutter env number in pubspec.yaml

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/pablo/psb/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/pablo/Documents/projects/liquid_progress_indicator_v2/example"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.7.7/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/omar/Projects/studio_projects/liquid_progress_indicator_v2/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
